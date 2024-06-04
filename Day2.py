72adcf62804efadba357e24a487862afba7db557
bipin1210 <bartaulabipin46@gmail.com>
2024-06-04 12:03:48 +0545

    Second day

---
+++ Day2.py
@@ -0,0 +1,13 @@
+print ("Bipin")
+name="abc"
+print(type(name))
+print(len(name))
+print(name.find("b"))
+print(name.lower())
+print(name.upper())
+print (name.replace("b", "r"))
+y=3.14
+z="5"
+z=int(z)
+print(int(y))
+print(z*3)
